f905ee3956bbac647dd71336055d86ee99c9aa22
NatalieFlin <90685429+NatalieFlin@users.noreply.github.com>
2021-12-01 10:54:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(86007321, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86007321" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+		<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(86007321, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86007321" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="master.css">
+		<link rel="stylesheet" href="media.css">
+	<title>Гироскутер Электросамокат Сигвей Смарт техник смарт техника магазин электросамокатов гироскутер купить магазин гироскутеров магазин смарт техника отзывы техник смарт отзывы гироскутер фото гироскутер цена</title>
+	<meta name="description" content="Смарт техники My Giro самый лучший магазин в россии, гироскутер, сигвей, электросамокат, гироскутер купить, электросамокат купить, электросамокат дешево, электросамокат ремонт, электросамокат замена, смарт техника, магазин электросамокатов, магазин гироскутеров, гироскутер фото, гироскутер цена"/>
+	<meta name="keywords" content="гироскутер, электросамокат, сигвей, смарт техник, сигвей купить, сигвей фото, сигвей цена, электросамокат цена"/>
+</head>
+<body>
+<header>
+	<a name="top"></a>
+		<div class="logo">
+			<img src="img\logo2.png" class="logo2-img" alt="магазин электросамокатов магазин гироскутеров магазин сигвеев купить гироскутер"></img>
+		<div class="lol">
+		<p class="phone">+7 351 111 22 33</p>
+		<p><a href="tel:+7 351 111 22 33"><img class="pbutton" src="img\pngwing.com.png" alt="Телефон"></a></p>
+		<p class="call1" >Позвоните нам</p>
+		</div>
+          <div class="navbar"><!--создаю выдвижное меню-->
+  <div class="dropdown">
+    <button class="dropbtn">Меню
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+        <a href="catalog.html">Каталог</a>
+        <a href="index.html">Главная</a>
+        <a href="komanda.html">Команда</a>
+        <a href="oNas.html">О нас</a>
+    </div>
+  </div> 
+</div>
+</header>
+	<main class ="main">
+		<article class="main_content">
+		</article>		
+	</main>
+    	<div class="aboutUs">
+        
+			    <h2 id= "aboutShop">О магазине смарт техники</h2>
+		<p>o <a href="oNas.html" target="blank">Наш магазин смарт техники</a> работает с 2016 года. Хорошие техник смарт отзывы. Мы представлены в таких городах как Екатеринбург, Москва и Челябинск и многие другие. В среднем мы отправляем по 15-20 заказов в день в разные города и регионы по всей России. У нас вы можете посмотреть гироскутер фото</p>
+    		<p>o Мы имеем свои сервисные центры и собственных мастеров, благодаря которым, выполняем гарантийные работы качественно и в срок, ваш самокат, гироскутер или сигвей всегда будет на колёсах. Мастера выполнят диагностику и выполнят ремонт. У нас можно купить гироскутер, ещё можно купить электросамокат.</p>
+    </div>    
+    </div>
+<h2 id = "aboutUs">Наши преимущества</h2>
+		  <!-- блок слайдера -->
+  <div class="carousel-wrapper">
+    <span id="item-1"></span>
+    <span id="item-2"></span>
+    <span id="item-3"></span>
+	<span id="item-4"></span>
+    <!-- 1й элемент слайдера -->
+
+		<div class="carousel-item item-1">
+		  <p>Мы обслуживаем одинаково качественно как своих гарантийных клиентов, так и сторонние заказы.</p>
+		  <a class="arrow arrow-prev" href="#item-4"></a>
+		  <a class="arrow arrow-next" href="#item-2"></a>
+		</div>
+		<!-- 2й элемент слайдера -->
+		<div class="carousel-item item-2">
+		  <p>Если деталь вашего электротранспорта сломалась — в нашем центре ее не будут ремонтировать, а сразу заменят на новую.</p>
+		  <a class="arrow arrow-prev" href="#item-1"></a>
+		  <a class="arrow arrow-next" href="#item-3"></a>
+		</div>
+		<!-- 3й элемент слайдера -->
+		<div class="carousel-item item-3">
+		  <p>Мы работаем с крупной сетью сервисных центров, ее филиалы находятся в 70 городах России.</p>
+		  <a class="arrow arrow-prev" href="#item-2"></a>
+		  <a class="arrow arrow-next" href="#item-4"></a>
+		</div>
+		<!-- 4й элемент слайдера -->
+		<div class="carousel-item item-4">
+		  <p>Гарантия действует в течении 3 лет!</p>
+		  <a class="arrow arrow-prev" href="#item-3"></a>
+		  <a class="arrow arrow-next" href="#item-1"></a>
+  </div>
+  </div>
+   <h2 id= "aboutShop">Наши консульнтанты</h2>
+  <div class="poop">
+    <div class="consultants">
+    	<div>
+				<img src="img\resourses\dan.png" class="consultants img" alt="гироскутер">
+				<p>o Иванов Иван Иванович – квалифицированный специалист сервисного центра My Gyro. Всегда поможет поставить ваш самокат, гироскутер или сигвей на колёса</p>
+				</div>
+				<div>
+				<img src="img\resourses\mon.png" class="consultants img" alt="гироскутер">
+				<p>o Пупкин Василий Васильевич – старший консультант My Gyro. Подберёт вам достойный электротранспорт(гиоскутер, самокат или сигвей)</p>
+				</div>
+				<div>
+				<img src="img\resourses\hasbulla.png" class="consultants img" alt="гироскутер">
+				<p>o Захаров Андрей Борисович – менеджер-консультант My Gyro. Ваш путеводитель по миру электротранспорта</p>
+				</div>
+				</div>
+    </div>
+<div class="wrapper">
+			<div class="content">
+				<h2>Особенности моделей транспорта смарт техники</h2>
+				<div class="infoBlocks"  id ="el">
+        <div class="poop">
+          <h2>Электросамокат</h2>
+<!-- 		<div class="video">
+<video controls>
+<source src="img/Гироскутер vs электросамокат , что купить!.mp4" type="video/mp4">
+</video>
+		</div> -->
+          <p><a href = "https://ru.wikipedia.org/wiki/Электрический_самокат#:~:text=Электри́ческий%20самока́т%20(эле́ктросамока́т)%20—%20средство,с%201915%20по%201921%20год" target="_blank">Электросамокат</a> - современный вариант простого самоката. Для того чтобы на нем кататься, нужно всего один раз оттолкнуться ногой от земли, обеспечив стартовое ускорение. Дальше устройство будет везти хозяина самостоятельно за счет встроенного электромотора. Если выбирать между моноколесом или электросамокатом, последний будет гораздо безопаснее в эксплуатации за счет наличия 2 колес (т.е. 2 точек опоры) и ручки для опоры руками. Электросамокат, а также гироскутер и севей, можно купить в нашем интернет магазине.Крупные службы проката электросамокатов организованы в нескольких городах в Калифорнии (США), Германии и в Сингапуре. Например, компании «Bird» и «Lime» в США смогли привлечь сотни миллионов долларов инвестиции.
+
+</p>
+        </div>
+				</div>
+				<div class="infoBlocks"  id ="giro">
+        <div class="poop">
+											    <h2>Гироскутер</h2>
+        <p><a href = "https://ru.wikipedia.org/wiki/%D0%93%D0%B8%D1%80%D0%BE%D1%81%D0%BA%D1%83%D1%82%D0%B5%D1%80" target="_blank"> Гироскутер</a> выделяет собой систему из 2 колес, оказавшихся по бокам от наездника и соединенных поперечной планкой-корпусом. Изнутри корпуса находятся электродвигатели, аккумуляторные батареи и гироскопы, поддерживающие в горизонтальном положении площадку для ног.
+
+Гироскутеры ни разу не оснащаются ручками для рук. Устройства из 2 колес, поперечной площадки и управляющего столба имеют название sigvey и управляются манипуляциями руками.
+
+Оригинальность управления гироскутером произведено в том, именно собственно что для движения вперед наезднику надо наклониться вперед. Для торможения или же же движения назад - наклониться назад.
+</p>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/LPEnC7-I9JI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>							        
+	<div class="infoBlocks" id ="sig">
+    <div class="poop">
+        <h2>Сигвей</h2>
+        <p><a href = "https://ru.wikipedia.org/wiki/%D0%A1%D0%B5%D0%B3%D0%B2%D0%B5%D0%B9" target="_blank">Сигвей</a> представляет собой конструкцию из 2 колес, расположенных по бокам от наездника и соединенных поперечной планкой-корпусом. Внутри корпуса располагаются электродвигатели, аккумуляторные батареи и гироскопы, поддерживающие в горизонтальном положении площадку для ног.
+
+Гироскутеры никогда не оснащаются рукоятками для рук. Устройства из 2 колес, поперечной площадки и рулевого столба называются сигвеями и управляются манипуляциями руками.
+
+Особенность управления гироскутером состоит в том, что для движения вперед наезднику необходимо наклониться вперед. Для торможения или движения назад - наклониться назад.</p>
+        </div>
+</div>
+			</div>
+<h1 id="catalog">Смарт техника ,представленная в нашем магазине</h1>
+      <div class="container"> <!--фильтр товаров-->
+  <input type="radio" id="blue" name="color" /><!--переключатель-->
+  <label for="blue">Электросамокаты</label>
+  <input type="radio" id="red" name="color"/>
+  <label for="red">Гироскутеры</label>
+  <input type="radio" id="green" name="color"/>
+  <label for="green">Сигвеи</label>
+  <input type="radio" id="reset" name="color"/>
+  <label for="reset">Все</label>
+
+  <div class="tile blue"><!--1-й товар-->
+    <div class="bbb">
+    <img src="img\tov6.jpg" class="filtrimg">
+    <p class="sos">Электросамокат</p>
+    <p class="nnn">Yamato PES MINI</p>
+    <p class="lll">15900 рублей</p>
+  </div>
+</div>
+  <div class="tile red"> <!--2-й товар-->
+  <div class="bbb">
+    <img src="img\tov1.jpg" class="filtrimg">
+    <p class="sos">Гироскутер</p>
+    <p class="nnn">Smart Balance 6.5" Ver. 1</p>
+    <p class="lll">8990 рублей</p>
+  </div>
+  </div>
+  <div class="tile blue"><!--3-й товар-->
+  <div class="bbb">
+    <img src="img\tov7.jpg" class="filtrimg">
+    <p class="sos">Электросамокат</p>
+    <p class="nnn">KUGOO S2 (Jilong)</p>
+    <p class="lll">14900 рублей</p>
+  </div>
+  </div>
+  <div class="tile green"><!--4-й товар-->
+  <div class="bbb">
+    <img src="img\tov3.jpg" class="filtrimg">
+    <p class="sos">Сигвей</p>
+    <p class="nnn">Minirobot Mini PRO 36V Белый</p>
+    <p class="lll">15990 рублей</p>
+  </div>
+  </div>
+  <div class="tile red"><!--5-й товар-->
+  <div class="bbb">
+    <img src="img\tov3.jpg" class="filtrimg">
+    <p class="sos">Гироскутер</p>
+    <p class="nnn">KIWANO KO-X ES03</p>
+    <p class="lll">18900 рублей</p>
+  </div>
+  </div>
+  <div class="tile green"><!--6-й товар-->
+  <div class="bbb">
+    <img src="img\tov5.jpg" class="filtrimg">
+    <p class="sos">Сигвей</p>
+    <p class="nnn">Hili E+</p>
+    <p class="lll">39900 рублей</p>
+  </div>
+  </div>
+  <div class="tile green"><!--7-й товар-->
+  <div class="bbb">
+    <img src="img\tov4.jpg" alt="смарт техника" class="filtrimg">
+    <p class="sos">Сигвей</p>
+    <p class="nnn">Xiaomi Ninebot Mini</p>
+    <p class="lll">22900 рублей</p>
+  </div>
+  </div>
+</div>
+    </div>
+</body>
+</html>
